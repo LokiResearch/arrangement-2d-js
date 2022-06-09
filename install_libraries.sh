4c7fd420add4f5b541d2510aa5fa18544f6a9a9a
Axel Antoine <ax@AxMac.local>
2022-06-09 16:30:57 +0200

    ci edits

--- install_libraries.sh
+++ install_libraries.sh
@@ -13,7 +13,7 @@ CGAL_URL=https://github.com/CGAL/cgal/releases/download/v$CGAL_VERSION/$CGAL_NAM
 echo "Downloading "$CGAL_NAME
 wget -nc -q --show-progress -O libraries/downloads/$CGAL_NAME $CGAL_URL
 echo "Extracting"
-pv libraries/downloads/$CGAL_NAME | tar -xz -C libraries
+tar -xf libraries/downloads/$CGAL_NAME -C libraries
 echo "Done"
 
 
@@ -26,7 +26,7 @@ BOOST_URL=https://boostorg.jfrog.io/artifactory/main/release/$BOOST_VERSION/sour
 echo "Downloading "$BOOST_NAME
 wget -nc -q --show-progress -O libraries/downloads/$BOOST_NAME $BOOST_URL
 echo "Extracting"
-pv libraries/downloads/$BOOST_NAME | tar -xz -C libraries
+tar -xf libraries/downloads/$BOOST_NAME -C libraries
 echo "Done"
 
 
